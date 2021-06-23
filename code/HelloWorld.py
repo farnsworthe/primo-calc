c10b1052a5e8be271460e3e30c45e7bdb858731d
muoyu <jun.mh@outlook.com>
2021-06-23 17:24:06 -0400

    created dir templates, moved folders

--- code/HelloWorld.py
+++ code/HelloWorld.py
@@ -1,11 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
+from app import app
+from flask import render_template
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-   
