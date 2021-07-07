3b3a559f404b310f1d5faa87ec0ba0c32e2316bb
muoyu <jun.mh@outlook.com>
2021-07-07 17:41:55 -0400

    modified .py, .css, .html

--- code/HelloWorld.py
+++ code/HelloWorld.py
@@ -1,9 +1,19 @@
 from flask import Flask
 app = Flask(__name__)
-from flask import render_template
+from flask import render_template, request, redirect
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    return render_template("index.html")
+
+@app.route("/", methods=["GET", "POST"])
+def data():
+    if request.method == "POST":
+
+        req = request.form
+        print(req)
+        return redirect(request.url)
+
     return render_template("index.html")
 
 if __name__ == '__main__':
